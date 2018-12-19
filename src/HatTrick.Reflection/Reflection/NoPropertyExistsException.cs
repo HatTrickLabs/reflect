e89f3cbf195af8f6b83d17c344d4db69f13fa893
JRod-Eiman <jerrod.eiman@gmail.com>
2018-12-19 17:54:23 -0600

    baseline commit

---
+++ src/HatTrick.Reflection/Reflection/NoPropertyExistsException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HatTrick.Reflection
+{
+    public class NoPropertyExistsException : Exception
+    {
+        public NoPropertyExistsException(string message) : base(message)
+        {
+        }
+    }
+}
