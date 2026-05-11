cab7cb41cb95b6f1d102c6a4375b8fb1c9ed6a35
Jerrod Eiman <jerrod.eiman@hattricklabs.com>
2026-05-11 09:23:55 -0500

    Exploring member info cache

--- src/HatTrick.Reflection/Expression.cs
+++ src/HatTrick.Reflection/Expression.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace HatTrick.Reflection
@@ -21,6 +23,8 @@ namespace HatTrick.Reflection
         #region expression [class]
         public static class Expression
         {
+            private static readonly ConcurrentDictionary<Type, Dictionary<string, MemberInfo>> _memberCache = new();
+
             #region reflect item
             public static T ReflectItem<T>(object source, string expression, bool throwOnNoItemExists = true)
             {
@@ -38,14 +42,12 @@ namespace HatTrick.Reflection
                 var exists = false;
 
                 int dotIdx = expression.IndexOf('.');
+                ReadOnlySpan<char> nameSpan = dotIdx > -1 ? expression.Slice(0, dotIdx) : expression;
 
-                string name = (dotIdx > -1) 
-                    ? expression.Slice(0, dotIdx).ToString()
-                    : expression.ToString();
-
-                //attempt dictionary lookup
+                //AlternateLookup is not available on the interface...
                 if (source is IDictionary idict)
                 {
+                    string name = nameSpan.ToString();
                     if (idict.Contains(name))
                     {
                         exists = true;
@@ -54,36 +56,51 @@ namespace HatTrick.Reflection
                 }
 
                 if (!exists)
-                {   
-                    //check for a property
+                {
                     Type t = source.GetType();
-                    PropertyInfo p = t.GetProperty(name);
-                    if (p != null)
+
+                    if (!_memberCache.TryGetValue(t, out var members))
+                    {
+                        members = BuildMemberMap(t);
+                        _memberCache.TryAdd(t, members);
+                    }
+
+#if NET9_0_OR_GREATER
+                    if (members.GetAlternateLookup<ReadOnlySpan<char>>().TryGetValue(nameSpan, out var member))
+#else
+                    if (members.TryGetValue(nameSpan.ToString(), out var member))
+#endif
                     {
                         exists = true;
-                        source = p.GetValue(source, null);
-                    }
-                    else //check for a field
-                    {
-                        FieldInfo f = t.GetField(name);
-                        if (f != null)
-                        {
-                            exists = true;
-                            source = f.GetValue(source);
-                        }
+                        source = member is PropertyInfo p
+                            ? p.GetValue(source, null)
+                            : ((FieldInfo)member).GetValue(source);
                     }
                 }
 
                 if (exists && source != null && dotIdx > -1)
-                {
-                    //recursive call...
                     source = ReflectItem(source, expression.Slice(++dotIdx, expression.Length - dotIdx), throwOnNoItemExists);
-                }
 
                 if (!exists && throwOnNoItemExists)
-                    throw new NoItemExistsException($"Argument '{nameof(source)}' of type '{source.GetType().FullName}' does not contain a property, field or dictionary key of: '{name}'");
+                    throw new NoItemExistsException($"Argument '{nameof(source)}' of type '{source.GetType().FullName}' does not contain a property, field or dictionary key of: '{nameSpan.ToString()}'");
 
                 return exists ? source : null;
+            }
+
+            private static Dictionary<string, MemberInfo> BuildMemberMap(Type t)
+            {
+                var map = new Dictionary<string, MemberInfo>(StringComparer.Ordinal);
+                foreach (FieldInfo f in t.GetFields())
+                {
+                    map[f.Name] = f;
+                }
+
+                foreach (PropertyInfo p in t.GetProperties())
+                {
+                    map[p.Name] = p; // properties shadow fields with the same name
+                }
+
+                return map;
             }
             #endregion
         }
