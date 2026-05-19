96d5b5a91fbbbd75596ce499240babef5f674c9d
Jerrod Eiman <jerrod.eiman@hattricklabs.com>
2026-05-19 09:54:35 -0500

    Rename alternate files.

---
+++ src/HatTrick.Reflection/AlternateLookupKey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HatTrick.Reflection
+{
+    public readonly ref struct AlternateLookupKey
+    {
+        #region interface
+        public readonly Type Type;
+        public readonly ReadOnlySpan<char> Name;
+        #endregion
+
+        #region ctor
+        public AlternateLookupKey(Type type, ReadOnlySpan<char> name)
+        {
+            Type = type;
+            Name = name;
+        }
+        #endregion
+    }
+}
